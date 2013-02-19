92e3e667861d5c82a0ba5a5e3ac0a67a9084bf40
JeanMary LECOUTEUX <jeanmary@gmail.com>
2013-02-19 14:29:27 +0100

    gestion du scroll et blocage de la taille des images

--- js/lightbox_brocoli.js
+++ js/lightbox_brocoli.js
@@ -3,26 +3,29 @@ $().ready(function() {
   $('.lb_brocoli').bind('click touch', function(e)
     {
       $('body').append('<div id="popin"></div>');
-      $('#popin').css('height', Math.max($('body').height(), $(window).innerHeight()));
+      $('#popin').css('height', Math.max($('body').height(), $(window).innerHeight()) + 200);
 
       $('#popin').append('<div id="content_popin"><p>CHARGEMENT EN COURS</p></div>');
       $('#content_popin').prepend('<div id="close">FERMER</div>');
-      $('#popin #close').bind('click', function()
-			{
-			  $('#popin').remove();
-		  });
+      $('#content_popin').css('margin-top', $(window).scrollTop() + 10);
 
       var img = new Image();
       img.src = e.currentTarget.getAttribute('ref');
       $(img).load(function()
         {
           $('#popin p').replaceWith(img);
+          $('#popin IMG').css('max-height', Math.round(document.body.clientHeight - 30));
+
         }
       );
+
+      $('#popin #close').bind('click', function()
+			{
+			  $('#popin').remove();
+		  });
+
 
       return false;
     }
   );
-
-
 });
